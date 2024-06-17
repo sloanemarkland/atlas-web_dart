a6ef3739d47a8d762b22fce1eb20d95c54bd09b5
Sarah Markland <51174002+sarahmarkland@users.noreply.github.com>
2024-06-17 15:32:06 -0500

    task 0 v2 - remove main func from working file, import util file

--- 0x03-asynchronous/0-users_count.dart
+++ 0x03-asynchronous/0-users_count.dart
@@ -1,17 +1,8 @@
 // create the usersCount function that prints the num of users
-import 'dart:async';
+import '0-util.dart';
 
 Future<void> usersCount() async {
   final userCount = await fetchUsersCount();
 
   print('$userCount');
-}
-
-Future<int> fetchUsersCount() => Future.delayed(
-      const Duration(seconds: 2),
-      () => 19,
-    );
-
-void main() async {
-  await usersCount();
 }
